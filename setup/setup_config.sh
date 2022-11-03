b11e4d6df435774b7cd3ffe4febebf47b3d1ad99
Eylon Goor <aylong11@gmail.com>
2022-11-03 17:56:28 +0200

    fix bugs

--- setup/setup_config.sh
+++ setup/setup_config.sh
@@ -3,12 +3,15 @@
 echo "Setting Project ID: ${GOOGLE_CLOUD_PROJECT}"
 gcloud config set project ${GOOGLE_CLOUD_PROJECT}
 
-echo "Editing google-ads.yaml file..."
-sed -i 's/MCC-ID/'${MCC_ID}'/' google-ads.yaml
-sed -i 's/YOUR-CLIENT-ID/'${OAUTH_CLIENT_ID}'/' google-ads.yaml
-sed -i 's/YOUR-CLIENT-SECRET/'${OAUTH_CLIENT_SECRET}'/' google-ads.yaml
-sed -i 's/YOUR-REFRESH-TOKEN/'${REFRESH_TOKEN}'/' google-ads.yaml
-sed -i 's/DEVELOPER-TOKEN/'${DEVELOPER_TOKEN}'/' google-ads.yaml
+echo "Creating google-ads.yaml file..."
+echo "
+login_customer_id: ${MCC_ID}
+client_id: ${OAUTH_CLIENT_ID}
+client_secret: ${OAUTH_CLIENT_SECRET}
+refresh_token: ${REFRESH_TOKEN}
+developer_token: ${DEVELOPER_TOKEN}
+use_proto_plus: 'True'
+" >> google-ads.yaml
 
 echo "Editing config.yaml file..."
 sed -i 's/YOUR-BQ-PROJECT/'${GOOGLE_CLOUD_PROJECT}'/g' config.yaml
