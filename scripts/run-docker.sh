9f96b8a141261f76846902fc138dda9ba4b4987a
Eylon Goor <aylong11@gmail.com>
2022-11-06 12:32:45 +0200

    move files to CS

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -2,7 +2,9 @@
 ads_queries=$1
 bq_queries=$2
 ads_yaml=$3
-gaarf $ads_queries -c=config.yaml --ads-config=$ads_yaml
+storage_path=gs://${GOOGLE_CLOUD_PROJECT}-urp
+
+gaarf $ads_queries -c=$storage_path/config.yaml --ads-config=$storage_path/$ads_yaml
 python3 conv_lag_adjustment.py -c=config.yaml
 gaarf-bq $bq_queries/views_and_functions/*.sql -c=config.yaml
 gaarf-bq $bq_queries/snapshots/*.sql -c=config.yaml
--- setup/setup_config.sh
+++ setup/setup_config.sh
@@ -3,6 +3,12 @@
 echo "Setting Project ID: ${GOOGLE_CLOUD_PROJECT}"
 gcloud config set project ${GOOGLE_CLOUD_PROJECT}
 
+echo "Enabling Cloud Storage service..."
+gcloud services enable storage-component.googleapis.com 
+
+echo "Creating cloud storage bucket..."
+gcloud alpha storage buckets create gs://${GOOGLE_CLOUD_PROJECT}-urp --project=${GOOGLE_CLOUD_PROJECT}
+
 echo "Creating google-ads.yaml file..."
 echo "
 login_customer_id: ${MCC_ID}
@@ -17,6 +23,11 @@ echo "Editing config.yaml file..."
 sed -i 's/YOUR-BQ-PROJECT/'${GOOGLE_CLOUD_PROJECT}'/g' config.yaml
 sed -i 's/MCC-ID/'${MCC_ID}'/g' config.yaml
 
+echo "Uploading google-ads.yaml file to cloud storage bucket..."
+gcloud alpha storage cp ./google-ads.yaml gs://${GOOGLE_CLOUD_PROJECT}-urp
+
+echo "Uploading config.yaml in cloud storage..."
+gcloud alpha storage cp ./config.yaml gs://${GOOGLE_CLOUD_PROJECT}-urp
 
 cat ./google-ads.yaml
 cat ./config.yaml
