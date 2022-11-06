4bcb067779c107aecb0046091a7a416dd054f99c
Eylon Goor <aylong11@gmail.com>
2022-11-06 14:26:05 +0200

    revert to read files locally

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -2,11 +2,7 @@
 ads_queries=$1
 bq_queries=$2
 ads_yaml=$3
-echo ${GOOGLE_CLOUD_PROJECT}
-echo $GOOGLE_CLOUD_PROJECT
-storage_path=gs://one-click-367809-urp
-
-gaarf $ads_queries -c=$storage_path/config.yaml --ads-config=$storage_path/$ads_yaml
+gaarf $ads_queries -c=config.yaml --ads-config=$ads_yaml
 python3 conv_lag_adjustment.py -c=config.yaml
 gaarf-bq $bq_queries/views_and_functions/*.sql -c=config.yaml
 gaarf-bq $bq_queries/snapshots/*.sql -c=config.yaml
