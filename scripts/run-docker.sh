d75d4aaefe11561f010c535115fa25f9a26946a6
Eylon Goor <aylong11@gmail.com>
2022-11-06 15:35:24 +0200

    add ads config path to conv lag adj

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -3,7 +3,7 @@ ads_queries=$1
 bq_queries=$2
 ads_yaml=$3
 gaarf $ads_queries -c=/config.yaml --ads-config=$ads_yaml
-python3 conv_lag_adjustment.py -c=/config.yaml
+python3 conv_lag_adjustment.py -c=/config.yaml --ads-config=$ads_yaml
 gaarf-bq $bq_queries/views_and_functions/*.sql -c=/config.yaml
 gaarf-bq $bq_queries/snapshots/*.sql -c=/config.yaml
 gaarf-bq $bq_queries/*.sql -c=/config.yaml
