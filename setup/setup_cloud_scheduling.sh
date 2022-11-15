63f08b079654721e27d6f173ea0f4bf7a600a652
sareetb <sareetb@gmail.com>
2022-11-15 12:07:25 +0200

    backslash missing

--- setup/setup_cloud_scheduling.sh
+++ setup/setup_cloud_scheduling.sh
@@ -11,7 +11,7 @@ echo -n -e "${COLOR}Creating Service Account..."
 gcloud iam service-accounts create $service_account_name --display-name $service_account_name 
 gcloud run services add-iam-policy-binding urp \
    --member=serviceAccount:$service_account_email \
-   --role=roles/run.invoker
+   --role=roles/run.invoker \
    --region=${GOOGLE_CLOUD_REGION}
 gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
      --member=serviceAccount:$service_account_email\
