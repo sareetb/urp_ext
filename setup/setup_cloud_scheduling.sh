35fc01e39cd9e0512bdc259a7f882aaefd14ffcb
sareetb <sareetb@gmail.com>
2022-11-15 10:16:48 +0200

    reverted past change and added region flag to add iam policy binding command

--- setup/setup_cloud_scheduling.sh
+++ setup/setup_cloud_scheduling.sh
@@ -9,9 +9,10 @@ subscription="urp-trigger-sub"
 echo -n -e "${COLOR}Creating Service Account..."
 
 gcloud iam service-accounts create $service_account_name --display-name $service_account_name 
-gcloud run services add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
+gcloud run services add-iam-policy-binding urp \
    --member=serviceAccount:$service_account_email \
    --role=roles/run.invoker
+   --region=${GOOGLE_CLOUD_REGION}
 gcloud projects add-iam-policy-binding ${GOOGLE_CLOUD_PROJECT} \
      --member=serviceAccount:$service_account_email\
      --role=roles/iam.serviceAccountTokenCreator
